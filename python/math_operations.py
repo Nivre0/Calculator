9132337b1981cddfb0286489ecbba1dbddcd46eb
tleuenberger <tleuenberger@kpmg.com>
2024-11-07 15:55:37 +0100

    basic operations in new file

---
+++ python/math_operations.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return x + y
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "ERROR: By Zero division"
+    return a / b
